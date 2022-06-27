248f6674c81e082455481e242850cfd38197d973
AlessHub <aless312@gmail.com>
2022-06-27 10:39:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+async function test() {
+    const config = {
+        headers: {
+          "Accept": "application/json"
+        }
+      }
+    const res = await fetch('https://disease.sh/v3/covid-19/all', config);
+    const result = await res.json();
+    console.log(result);
+  }
+
+test()
