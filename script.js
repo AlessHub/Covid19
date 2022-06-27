24b4e937dfc88859704b0d366c2fe58ebbc474ed
AlessHub <aless312@gmail.com>
2022-06-27 11:16:32 +0200

    Añadido imagenes

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ async function test() {
     const res = await fetch('https://disease.sh/v3/covid-19/all', config);
     const result = await res.json();
     console.log(result);
+    stat1.innerHTML = result.active.stat1
   }
 
 test()
