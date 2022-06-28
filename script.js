901edb8b769ace4a72398f289a3e84df9f2dcb6b
AlessHub <aless312@gmail.com>
2022-06-28 09:18:03 +0200

    script update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async function test() {
+async function getData() {
     const config = {
         headers: {
           "Accept": "application/json"
@@ -7,7 +7,38 @@ async function test() {
     const res = await fetch('https://disease.sh/v3/covid-19/all', config);
     const result = await res.json();
     console.log(result);
-    stat1.innerHTML = result.active.stat1
+    console.log(result.active);
+    createDataCard(result);
   }
 
-test()
+getData()
+
+// Poner html dentro de const
+
+// const createDataCard = (result) => {
+//   const cardHTML = `
+//         <div class="container">
+//           <section class="stats">
+//               <span class="totalcase" id="stat1">Total Case ${result.cases}</span>
+//               <span class="activecase">Active Case ${result.active}</span>
+//               <span class="recoveredcase">Recovered Case ${result.recovered}</span>
+//               <span class="deathcase">Death Case ${result.deaths}</span>
+//           </section>
+//         </div>
+//       `;
+//   main.innerHTML = cardHTML;
+// };
+
+
+// funcion para conseguir data de pais especifico
+
+// async function getCountryData() {
+//   const config = {
+//     headers: {
+//       "Accept": "application/json"
+//     }
+//   }
+//   const res = await fetch('#', config);
+//   const result = await res.json();
+  
+// }
