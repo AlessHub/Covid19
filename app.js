6b0833255f7a068812d1829c5ab05bba27680945
Eberth <itec_19@hotmail.com>
2022-07-19 20:03:34 +0200

    ajaxtoapi

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 
 // ajax table
 
-$(document).ready( function () {
+$(document).ready(async function () {
+  let mydata = await fetch("https://disease.sh/v3/covid-19/countries?sort=cases")
+  .then(response => response.json());
     $('#example').DataTable({
-      'ajax' : 'data.json',
-      'columns' : [
-          {'data' : 'country'},
+      data : mydata,
+      columns : [
+          { data : 'countryInfo.flag', 
+            render: function (data, type){
+                return `<img class="imagetable" src="${data}">` 
+              }
+          },
           {'data' : 'country'},
           {'data' : 'cases'},
+
           {'data' : 'oneCasePerPeople'},
           {'data' : 'deaths'},
           {'data' : 'oneDeathPerPeople'}, 
